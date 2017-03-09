000b8057f7d7542663f6a475b80509fd4782ca37
Alasdair McLeay <builds@travis-ci.com>
2017-03-09 20:59:59 +0100

    Exercise 2

--- src/components/patterns/RenderCallback/exercise/RenderCallback/index.jsx
+++ src/components/patterns/RenderCallback/exercise/RenderCallback/index.jsx
@@ -6,3 +6,35 @@ Hint:
   here and receive the arguments from GitHubIssueSearch using an arrow function
 
 */
+
+import React from "react"
+import GitHubIssueSearcher from "./GitHubIssueSearcher"
+import ScrollNotifier from '../ScrollNotifier'
+
+const RenderCallback = () => (
+  <GitHubIssueSearcher>
+    {(issues, fetchNextPage, isFetching) => (
+      <ScrollNotifier
+        style={{ height: 400 }}
+        onScrollAtTheBottom={ fetchNextPage }
+      >
+        <ul className="issue-list">
+          {issues.map(item => (
+            <li key={item.id}>
+              <a href={item.html_url} target="_blank">
+                <h3>{item.title}</h3>
+                <p>{item.body}</p>
+              </a>
+            </li>
+          ))}
+          {!isFetching ? null : <li className="text-center" key="loading">
+              <h2><i className="fa fa-cog fa-spin fa-3x fa-fw margin-bottom"></i></h2>
+            </li>
+          }
+        </ul>
+      </ScrollNotifier>
+    )}
+  </GitHubIssueSearcher>
+)
+
+export default RenderCallback
